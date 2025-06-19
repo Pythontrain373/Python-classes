e68c2f6d7a324e7d149193a472a2bdc81a9492d1
Pythontrain373 <atikashwani@gmail.com>
2025-06-19 21:12:16 +0800

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,26 @@
+"""Print Statements and Comments
+Outline:
+In this activity, students will learn the basics of Python programming by using print statements to display text
+and perform simple calculations. They will also learn how to use comments to document their code."""
+#print("welcome to the class")
+#print("it is my first python class.\n\ntoday is tuseday")
+"""a=10
+b=20
+print("The sum of the numbers =",a+b)
+c=50
+d=30
+print("The result is =",c-d)"""
+
+
+"""Algorithm (Legacy Activity)
+Outline:
+You have to write step by step instructions on how the addition is performed
+print("Step 1= declare variable number1=10")
+print("Step 2= declare variable number2=20")
+print("Step 3= add number1+number2=30")
+print("Step 4= print the output")"""
+
+
+"""Flowchart (Legacy Activity)
+Outline:
+You have to draw a flowchart to check the lamp is working or not."""

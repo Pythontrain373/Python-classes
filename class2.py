52c809d5c274ccf13738a42fc10fdcfdb62c760e
Pythontrain373 <atikashwani@gmail.com>
2025-06-21 18:13:21 +0800

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,42 @@
+"""Welcome to the world of coding
+Outline:
+Write a program for print command."""
+
+#print("Welcome to the world of programming!")
+
+
+"""Understanding the variables
+Outline:
+Write a program to create different variables to store various values.
+
+
+name="laptop"
+print(name)
+age=10
+print(age)
+print("i like",name)
+a=input("What is your name: ")
+print("my name is",a)
+b=int(input("What is your age?: "))
+print("my age is",b)"""
+
+
+"""Keywords
+Outline:
+Write a program to print all the keywords
+
+
+import keyword
+print(keyword.kwlist)"""
+
+
+name=input("What is your name?: ")
+age=input("What is your age?: ")
+school=input("What school do you go to?: ")
+dream=input("What is your dream?: ")
+print("now i am going to make a paragraph on you based on the questions before  :)")
+print("Hi my name is",name)
+print
+print("I go to",school)
+print("My dream is",dream)
+

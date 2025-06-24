d9c7da17b07c38ad3e27bc405afe9c03454bd9f7
Pythontrain373 <atikashwani@gmail.com>
2025-06-24 14:33:59 +0800

    Add files via upload

---
+++ class2project.py
@@ -0,0 +1,10 @@
+benbirthday="2/11"
+tombirthday="5/7"
+jackbirthday="19/8"
+ronnybirthday="23/5"
+aidenbirthday="25/2"
+print("ben's birthday is on",benbirthday)
+print("tom's birthday is on",tombirthday)
+print("jack's birthday is on",jackbirthday)
+print("ronny's birthday is on",ronnybirthday)
+print("aiden's birthday is on",aidenbirthday)

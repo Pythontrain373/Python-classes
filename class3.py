9353504447897064f939b715a165b4755157f5de
Pythontrain373 <atikashwani@gmail.com>
2025-06-24 21:05:45 +0800

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,62 @@
+"""Data types
+Outline:
+Write a program to find what type of datatype the given variable is?"""
+
+
+"""a="coding"
+print(type(a))
+b=10
+print(type(b))
+c=5.3
+print(type(c))
+d=True
+print(type(d))"""
+
+
+"""Typecasting
+Outline:
+Write a program to change the datatype of given variables?
+
+
+b=10
+c=str(b)
+print(type(c))
+
+
+a=3.4
+d=str(a)
+print(type(d))
+
+a="apple"
+b=int(a)
+print(type(b))
+
+
+a=1.5
+b=int(a)
+print(type(b))
+
+
+String operation
+Outline:
+Write a program to print the “CODINGAL” word in a reverse direction."""
+
+
+
+a="Codingal "
+print(a[2:5])
+
+
+d="comfortable"
+print(d[3:6])
+print(d[-7:-4])
+print(d[6:])
+print(d[0::])
+print(d.upper())
+
+
+
+print(a.upper()[::-1])
+
+
+print(a+d)

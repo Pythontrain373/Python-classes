eb81696cb03deae7fc74ea40f4dbcf58b5162528
Pythontrain373 <atikashwani@gmail.com>
2025-06-28 18:09:29 +0800

    Add files via upload

---
+++ class5.py
@@ -0,0 +1,46 @@
+"""Conditional statements
+Outline:
+write a program to check whether the given number is positive?
+
+
+number=int(input("Enter the number: "))
+if number>0:
+    print("Number is positive")
+else:
+    print("Number is negetive")
+
+
+Profit loss
+Outline:
+Write to check if a person is buying oranges at 100 rs and later selling it 1at 120 rs. Find that he has profit or loss?
+
+
+cost=int(input("How much did you buy it for: "))
+sell=int(input("How muck did you sell it for: "))
+if cost>sell:
+    print("You have a loss of",cost-sell,"rs")
+else:
+    print("You have a profit of",sell-cost,"rs")
+
+
+Checking the number greater or smaller
+Outline:
+Write a program to check whether the given number is greater than 15 or smaller than 15.
+
+
+a=15
+b=int(input("What number do you want to check 15 with: "))
+if b>a:
+    print("the number is greater than 15")
+else:
+    print("The number is smaller than 15")
+
+    Odd-even
+Outline:
+Write a program to check whether the given number is even or odd?
+
+number=int(input("What number do you want to pick: "))
+if number%2==0:
+    print("It is a even number")
+else:
+    print("it is a odd number")"""

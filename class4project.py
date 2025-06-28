0ca9ba0ab356ebf86782e3abc78f85cf8c2b0c3d
Pythontrain373 <atikashwani@gmail.com>
2025-06-28 14:32:06 +0800

    Add files via upload

---
+++ class4project.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+
+print("Welcome to the square root claculator :)")
+a=int(input("Please choose the number you want to calculate the square root of: "))
+answer=sqrt(a)
+print("The answer is",answer)

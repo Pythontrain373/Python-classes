af28c728f0c38d123b295f5c4d0358716b48a5f9
Pythontrain373 <atikashwani@gmail.com>
2025-06-29 19:53:15 +0800

    Add files via upload

---
+++ class5project.py
@@ -0,0 +1,12 @@
+from datetime import date
+if date:
+    tremprature="hot"
+    
+else:
+    tremprature="cold"
+
+
+if tremprature=="hot":
+    print("You can wear light cloths today  :)")
+else:
+    print("You need to wear a jacket today  :(")

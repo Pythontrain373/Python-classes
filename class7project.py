fc380f2a4274644b0175cab55d6e960dd06f603f
Pythontrain373 <atikashwani@gmail.com>
2025-07-05 23:51:18 +0800

    Add files via upload

---
+++ class7project.py
@@ -0,0 +1,6 @@
+print("Welcome to the ASCII calculator  :)")
+word=input("Please write a letter or a word: ")
+for char in word:
+    print(char,"- Corresponding ASCII value :",ord(char))
+
+"""Sorry mam i didn't really understand how to do the line 3 to 4 so I watched a youtube video  to learn how to do this"""

fc44ca7151139de4daac39647a8de6a6ab94262e
Pythontrain373 <atikashwani@gmail.com>
2025-07-08 17:11:08 +0800

    Add files via upload

---
+++ class8project.py
@@ -0,0 +1,6 @@
+print("Welcome to the number swapper :)")
+a=int(input("What is the first number: "))
+b=int(input("What is the second number: "))
+c=int(input("What is the second number: "))
+a,b,c=c,b,a
+print("After swapping: Your first number=", a, ",Your second number=", b,"Your third number=",c)

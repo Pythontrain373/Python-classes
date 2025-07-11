fecdd65a25c5b2d90f9f50671cb718f62e6484e4
Pythontrain373 <atikashwani@gmail.com>
2025-07-11 12:38:31 +0800

    Add files via upload

---
+++ class10.py
@@ -0,0 +1,37 @@
+"""Sum of whole numbers
+Outline:
+Write a program to calculate the sum of whole numbers.
+
+
+for i in range(50,1,-3):
+    print(i)
+
+
+number=int(input("Please enter the number: "))
+sum=0
+for i in range(1,number+1):
+    sum=sum+i
+print(sum)
+
+
+Reverse a String
+Outline:
+Write a program to reverse the string entered by the user.
+
+
+name=input("What is your name: ")
+reverse=""
+for i in name:
+    reverse=i+reverse
+print(reverse)
+
+
+
+reverse order
+Outline:
+Write a program to print the numbers in reverse order beginning from the number entered by the user.
+
+
+number=int(input("What is the number: "))
+for i in range(number,0,-1):
+    print(i)"""

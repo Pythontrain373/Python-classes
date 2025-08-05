0d360e6e9f07c3d18c0ffae1c6dfa98ed31d1922
Pythontrain373 <atikashwani@gmail.com>
2025-08-05 10:18:32 +0800

    Add files via upload

---
+++ class22project.py
@@ -0,0 +1,8 @@
+def multiply_tuple_numbers(numbers):
+    result = 1
+    for number in numbers:
+        result=result*number
+    return result
+my_tuple=(1,2,3,4,5)
+product=multiply_tuple_numbers(my_tuple)
+print(product)

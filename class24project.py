d7fe4df95ee20443653ff96fe70a194a0b3e3b3e
Pythontrain373 <atikashwani@gmail.com>
2025-08-07 18:37:55 +0800

    class24project.py

--- class24project.py
+++ class24project.py
@@ -1,7 +1,6 @@
-def set_difference(set_a,set_b):
-    return set_a - set_b
-set_a={1, 2, 3, 4, 5}
-set_b={4, 5, 6, 7, 8}
-difference=set_difference(set_a, set_b)
+def difference(set1,set2):
+    return set1-set2
+set1={1, 2, 3, 4, 5}
+set2={4, 5, 6, 7, 8}
+difference=difference(set1,set2)
 print(difference)
-

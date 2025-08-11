36db4033673d23bbdd14c62af94848766004da87
Pythontrain373 <atikashwani@gmail.com>
2025-08-11 14:08:45 +0800

    class25project.py

--- class25project.py
+++ class25project.py
@@ -5,13 +5,14 @@ try:
     else:
         odd_numbers_list_1=[i for i in range(1, user_input) if i%2!=0]
         odd_numbers_list_2=odd_numbers_list_1.copy()
-        print("First list of odd numbers: ", odd_numbers_list_1)
-        print("Second list of odd numbers: ", odd_numbers_list_2)
+        print("First list of odd numbers: ",odd_numbers_list_1)
+        print("Second list of odd numbers: ",odd_numbers_list_2)
 except ValueError:
     print("Invalid input.Please enter an number.")
 fruits_list=["apple", "banana", "cherry", "dates"]
 capitalized_fruits_list=[fruit.capitalize() for fruit in fruits_list]
 print("Original fruits list: ",fruits_list)
 print("Capitalized fruits list: ",capitalized_fruits_list)
+
 
 #line 6,7 and 13 were copied form google pls explane next class

5fb129d7bf099fd7eb3b10ca3b2fd6bd3f0fec45
Pythontrain373 <atikashwani@gmail.com>
2025-08-25 11:12:42 +0800

    Add files via upload

---
+++ class30project.py
@@ -0,0 +1,9 @@
+class StringReverse:
+    def reverse_words(self, s):
+        self.s=s
+        s=word
+        return ' '.join(s[::-1])
+hi=StringReverse()
+word=input("What is the word: ")
+hi.reverse_words(word)
+#Can you tell me why it is not working
